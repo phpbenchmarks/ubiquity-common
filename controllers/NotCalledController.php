580c39e12f5e13ff4f98bba67c4dcaa8763dedf8
jcheron <myaddressmail@gmail.com>
2018-12-29 01:43:17 +0100

    Create NotCalledController for the 500 routes

---
+++ controllers/NotCalledController.php
@@ -0,0 +1,12 @@
+<?php
+namespace PhpBenchmarksUbiquity\RestApi\controllers;
+ /**
+ * Controller NotCalledController
+ **/
+class NotCalledController extends \Ubiquity\controllers\Controller{
+
+	public function index(){
+		echo "not to call!";
+	}
+
+}
