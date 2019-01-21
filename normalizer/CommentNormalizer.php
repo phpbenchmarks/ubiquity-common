dce97faed5a180247a2d6ea8ed61ec8668afc24b
jcheron <myaddressmail@gmail.com>
2019-01-21 18:19:56 +0100

    Usage of TranslatorManager instead of Translator

--- normalizer/CommentNormalizer.php
+++ normalizer/CommentNormalizer.php
@@ -2,22 +2,12 @@
 
 namespace PhpBenchmarksUbiquity\RestApi\normalizer;
 
-use Ubiquity\translation\Translator;
 use PhpBenchmarksRestData\Comment;
 use Ubiquity\contents\normalizers\NormalizerInterface;
 use Ubiquity\contents\normalizers\NormalizersManager;
+use Ubiquity\translation\TranslatorManager;
 
 class CommentNormalizer implements NormalizerInterface {
-	
-	/**
-	 * @var Translator
-	 */
-	private $translator;
-	
-	
-	public function __construct(Translator $translator){
-		$this->translator=$translator;
-	}
 
 	public function supportsNormalization($data) {
 		return $data instanceof Comment;
@@ -27,7 +17,7 @@ class CommentNormalizer implements NormalizerInterface {
 		return [
 				'id' => $object->getId(),
 				'message' => $object->getMessage(),
-				'translated' => $this->translator->trans('translated.2000', [], 'phpbenchmarks'),
+				'translated' => TranslatorManager::trans('translated.2000', [], 'phpbenchmarks'),
 				'type' => NormalizersManager::normalize_($object->getType())
 		];
 	}
